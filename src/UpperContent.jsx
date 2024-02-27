dca5d1938adf4e68de60cac9d5f06010efe48110
Prasanna <me210003054@iiti.ac.in>
2024-02-27 18:05:30 +0530

    final commit, moving images to public works

--- src/UpperContent.jsx
+++ src/UpperContent.jsx
@@ -13,7 +13,7 @@ export default function UpperContent (){
                     <li>C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for various projects across the globe to save energy.</li>
                     <li>C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel. </li>
                 </ul>
-                <img src="./essentials/accept.png" style={{width:"100%"}}/>
+                <img src="accept.png" style={{width:"100%"}}/>
                 <p>Government of India has awarded the "National Energy Conservation Award 2018". Mr. G. Selvaraj, Joint Managing Director of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of Lok Sabha & Shri. Raj Kumar Singh, Honorable Minister of State.</p>
             </div>
         </div>
